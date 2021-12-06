5668f6b24a25e394865225ac3f8e0dd013a3007f
Adam Weeden <adam.weeden@nrccua.org>
2021-12-06 11:02:02 -0500

    [E4E-0]: Flesh out unit tests

---
+++ lib/HeadersLink/index.test.ts
@@ -0,0 +1,46 @@
+import { NextLink, Operation } from '@apollo/client';
+import HeadersLink from '.';
+
+describe('HeadersLink', () => {
+  const mockForward = jest.fn();
+  const mockGetContext = jest.fn();
+  const mockOperation = {
+    getContext: mockGetContext,
+  } as unknown as Operation;
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+    mockForward.mockReturnValue([{}]);
+    mockGetContext.mockReturnValue({
+      restResponses: [{}],
+    });
+  });
+
+  afterAll(() => {
+    jest.restoreAllMocks();
+  });
+
+  it('can handle requests without headers', () => {
+    const headersLink = new HeadersLink();
+
+    const result = headersLink.request(mockOperation, mockForward as unknown as NextLink);
+
+    expect(result).toEqual([{ data: { headers: {} } }]);
+  });
+
+  it('can handle requests with headers', () => {
+    mockGetContext.mockReturnValue({
+      restResponses: [
+        {
+          headers: [['x-api-key', '12345']],
+        },
+      ],
+    });
+
+    const headersLink = new HeadersLink();
+
+    const result = headersLink.request(mockOperation, mockForward as unknown as NextLink);
+
+    expect(result).toEqual([{ data: { headers: { xApiKey: '12345' } } }]);
+  });
+});
